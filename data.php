e1fef5d2c839517c0e70a39777a925f5cd6fd506
rrrcr <83922058+rrrcr@users.noreply.github.com>
2021-05-12 00:24:27 +0200

    Reformated code

--- data.php
+++ data.php
@@ -1,17 +1,12 @@
 ﻿<?php
-
-main();
-
-function main()
+$db = new PDO('sqlite:/tmp/info.db');
+if (checkIfDataExists())
 {
-    if (checkIfDataExists())
-    {
-        showMessage();
-    }
-    else
-    {
-        prepareDatabase();
-    }
+    prepareDatabase();
+}
+else
+{
+    showMessage();
 }
 
 function checkIfDataExists()
@@ -19,13 +14,10 @@ function checkIfDataExists()
 
     try
     {
-        $db = new PDO('sqlite:/tmp/info.db');
-        $results = $db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='{facts}'");
-        while ($row = $results->fetch())
-        {
-            return true;
-        }
-        return false;
+        global $db;
+        $results = $db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='facts'");
+        $answer = $results->fetchAll();
+        return empty($answer);
     }
     catch(PDOException $e)
     {
@@ -36,7 +28,7 @@ function checkIfDataExists()
 
 function showMessage()
 {
-    $db = new PDO('sqlite:/tmp/info.db');
+    global $db;
     $info = $db->query("SELECT fact_string FROM facts WHERE DATE(date) BETWEEN DATE( 'now','-1 days') AND DATE( 'now')");
     $str = $info->fetchAll();
     if (empty($str))
@@ -61,57 +53,62 @@ function prepareDatabase()
 
 function createDatabase()
 {
-    $db = new PDO('sqlite:/tmp/info.db');
+    global $db;
     $db->exec("CREATE TABLE facts ( fact_id INTEGER PRIMARY KEY AUTOINCREMENT, fact_string VARCHAR(255),date DATETIME )");
 }
 
 function prepareFromJson()
 {
-    $db = new PDO('sqlite:/tmp/info.db');
+    global $db;
     $jsonString = file_get_contents("facts.json");
     $jsonString = str_replace('\"', '\\\\\"', $jsonString);
     $fields = json_decode($jsonString);
+
     foreach ($fields as $value)
     {
-        $db->exec("INSERT INTO facts (fact_string) VALUES ('" . $value->fact . "')");
+        $ex = $db->prepare("INSERT INTO facts (fact_string) VALUES (?)");
+        $ex->execute([$value->fact]);
     }
 }
 
 function prepareFromText()
 {
-    $db = new PDO('sqlite:/tmp/info.db');
+    global $db;
     $factsTxt = file_get_contents("facts.txt");
-    $factsArray = explode(". ", $factsTxt);
+    $factsArray = explode(chr(10) , $factsTxt);
     foreach ($factsArray as $fact)
     {
-        $db->exec("INSERT INTO facts (fact_string) VALUES ('" . $fact . "')");
+        $ex = $db->prepare("INSERT INTO facts (fact_string) VALUES (?)");
+        $ex->execute([$fact]);
     }
 
 }
 
 function prepareFromPhp()
 {
-    $db = new PDO('sqlite:/tmp/info.db');
+    global $db;
     $pattern = '/"([^"]+)"/';
     $factsPhp = file_get_contents("facts.php");
     if (preg_match_all($pattern, $factsPhp, $match))
     {
         foreach ($match[0] as $fact)
         {
-            $db->exec("INSERT INTO facts (fact_string) VALUES ('" . $fact . "')");
+            $ex = $db->prepare("INSERT INTO facts (fact_string) VALUES (?)");
+            $ex->execute([$fact]);
         }
     }
 }
 function updateDate()
 {
-    $db = new PDO('sqlite:/tmp/info.db');
+    global $db;
     $info = $db->query(" SELECT COUNT(*) FROM facts ");
     $str = $info->fetchAll();
     $numberOfRows = $str[0]['COUNT(*)'];
     $date = date('Y-m-d h:m:s');
     for ($i = 1;$i <= $numberOfRows;$i++)
     {
-        $db->exec("UPDATE facts SET date = '" . $date . "' WHERE fact_id = '" . $i . "'");
+        $ex = $db->prepare("UPDATE facts SET date = ?  WHERE fact_id = ?");
+        $ex->execute([$date, $i]);
         $date = date('Y-m-d h:m:s', strtotime("1 day", strtotime($date)));
     }
 }
